8812a716bd62f07e5fdef8c30ecd360895ec558d
demidraksin <greatraksin@icloud.com>
2021-05-12 10:07:07 +0300

    Adding hw solution🍕

--- homework_1205/main.py
+++ homework_1205/main.py
@@ -11,5 +11,20 @@ soup = BeautifulSoup(connect.text, features='html.parser')
 cards = soup.find_all('div', {'class': 'product-card'})  # найти все карточки товаров
 
 for item in cards:
-    print(item)
+    s = parse_dominos(item)
+    df = df.append(s, ignore_index=True)
 
+df.to_excel('dominos.xlsx')
+
+url2 = 'https://dominos.by/bread'
+connect2 = requests.get(url2)
+df2 = pd.DataFrame()
+soup2 = BeautifulSoup(connect2.text, features='html.parser')
+cards2 = soup2.find_all('div', {'class': 'product-card'})  # найти все карточки товаров
+
+for item in cards2:
+    print(item)
+    s = parse_dominos(item)
+    df2 = df2.append(s, ignore_index=True)
+
+df2.to_excel('bread.xlsx')
--- homework_1205/my_parser.py
+++ homework_1205/my_parser.py
@@ -3,6 +3,20 @@ import pandas as pd
 
 def parse_dominos(card):
     res = pd.DataFrame()  # таблица для результатов
+    url = 'https://dominos.by/pizza/'  # ссылка на страницу с пиццами
+    pizza_link = url + card.get('data-code').lower()  # ссылка на пиццу
+    pizza_name = card.find('div', {'class': 'product-card__title'}).text.strip()
+    # .text достает текст из тега <div class="product-card__title">
+    # метод .strip() убирает пробелы справа и слева от строки
+    pizza_description = card.find('div', {'class': 'product-card__description'}).text.strip()
+    pizza_img_link = card.find('img', {'class': 'product-card-media__element'}).get('src')
+    pizza_price = card.find('p', {'class': 'product-card__modification-info-price'}).text
+    pizza_weight = card.find('p', {'class': 'product-card__modification-info-weight'}).text
 
-
-    return None
+    res = res.append(
+        pd.DataFrame(  # создаю таблицу на основе полученных данных
+            [[pizza_link, pizza_name, pizza_description, pizza_price, pizza_weight, pizza_img_link]],
+            columns=['Ссылка', 'Название', 'Ингридиенты', 'Цена', 'Масса', 'картинка']),
+        ignore_index=True
+    )
+    return res
